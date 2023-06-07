751f11cb6c06bb45b49a604193d7f6dfea87dc6f
Orçun Aşık <asikorcun@gmail.com>
2023-06-07 15:54:04 +0300

    CustomerToken was created for customer login

---
+++ Core/Utilities/Security/JWT/CustomerToken.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Utilities.Security.JWT
+{
+    public class CustomerToken
+    {
+        public string CustomerAccessToken { get; set; }
+        public DateTime Expiration { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
