96face9812216d1a236cc5ffc3db308ec3b3a89c
Orçun Aşık <asikorcun@gmail.com>
2023-07-30 23:24:09 +0300

    Created have been UserDto

---
+++ Entities/Dtos/UserDto.cs
@@ -0,0 +1,10 @@
+﻿using Entities.Concrete;
+
+namespace Entities.Dtos
+{
+    public class UserDto : User
+    {
+        public string Password { get; set; }
+        public string? NewPassword { get; set; }
+    }
+}
