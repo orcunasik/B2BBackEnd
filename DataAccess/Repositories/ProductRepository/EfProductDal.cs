0d066dcb9f4781cfda2e5257053cb25f1c5f8b70
Orçun Aşık <asikorcun@gmail.com>
2023-08-07 16:16:46 +0300

    Added check by customerId.

--- DataAccess/Repositories/ProductRepository/EfProductDal.cs
+++ DataAccess/Repositories/ProductRepository/EfProductDal.cs
@@ -31,22 +31,42 @@ namespace DataAccess.Repositories.ProductRepository
         {
             using (SimpleContextDb context = new SimpleContextDb())
             {
-                CustomerRelationship customerRelationship = await context.CustomerRelationships.Where(c => c.CustomerId == customerId).SingleOrDefaultAsync();
-                IQueryable<ProductListDto> result = from product in context.Products
-                                                    select new ProductListDto
-                                                    {
-                                                        Id = product.Id,
-                                                        Name = product.Name,
-                                                        Discount = customerRelationship.Discount,
-                                                        Price = (context.PriceListDetails.Where(p => p.PriceListId == customerRelationship.PriceListId && p.ProductId == product.Id).Count() > 0
-                                                        ? context.PriceListDetails.Where(p => p.PriceListId == customerRelationship.PriceListId && p.ProductId == product.Id).Select(s => s.Price).SingleOrDefault()
-                                                        : 0),
-                                                        MainImageUrl = (context.ProductImages.Where(p => p.ProductId == product.Id && p.IsMainImage == true).Count() > 0
-                                                        ? context.ProductImages.Where(p => p.ProductId == product.Id && p.IsMainImage == true).Select(s => s.ImageUrl).FirstOrDefault()
-                                                        : ""),
-                                                        Images = context.ProductImages.Where(p => p.ProductId == product.Id).Select(s => s.ImageUrl).ToList()
-                                                    };
-                return await result.OrderBy(p => p.Name).ToListAsync();
+                if (customerId != 0)
+                {
+                    var customerRelationship = context.CustomerRelationships.Where(c => c.CustomerId == customerId).SingleOrDefault();
+                    var result = from product in context.Products
+                                                        select new ProductListDto
+                                                        {
+                                                            Id = product.Id,
+                                                            Name = product.Name,
+                                                            Discount = customerRelationship.Discount,
+                                                            Price = (context.PriceListDetails.Where(p => p.PriceListId == customerRelationship.PriceListId && p.ProductId == product.Id).Count() > 0
+                                                            ? context.PriceListDetails.Where(p => p.PriceListId == customerRelationship.PriceListId && p.ProductId == product.Id).Select(s => s.Price).SingleOrDefault()
+                                                            : 0),
+                                                            MainImageUrl = (context.ProductImages.Where(p => p.ProductId == product.Id && p.IsMainImage == true).Count() > 0
+                                                            ? context.ProductImages.Where(p => p.ProductId == product.Id && p.IsMainImage == true).Select(s => s.ImageUrl).FirstOrDefault()
+                                                            : ""),
+                                                            Images = context.ProductImages.Where(p => p.ProductId == product.Id).Select(s => s.ImageUrl).ToList()
+                                                        };
+                    return await result.OrderBy(p => p.Name).ToListAsync();
+                }
+                else
+                {
+                    var customerRelationship = context.CustomerRelationships.Where(c => c.CustomerId == customerId).SingleOrDefault();
+                    var result = from product in context.Products
+                                                        select new ProductListDto
+                                                        {
+                                                            Id = product.Id,
+                                                            Name = product.Name,
+                                                            Discount = 0,
+                                                            Price = 0,
+                                                            MainImageUrl = (context.ProductImages.Where(p => p.ProductId == product.Id && p.IsMainImage == true).Count() > 0
+                                                            ? context.ProductImages.Where(p => p.ProductId == product.Id && p.IsMainImage == true).Select(s => s.ImageUrl).FirstOrDefault()
+                                                            : ""),
+                                                            Images = context.ProductImages.Where(p => p.ProductId == product.Id).Select(s => s.ImageUrl).ToList()
+                                                        };
+                    return await result.OrderBy(p => p.Name).ToListAsync();
+                }
             }
         }
     }
